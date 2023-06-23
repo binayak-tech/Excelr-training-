96c6e1d27e1b4ecc87622e9cf024489c381ddd5c
Binayak Purohit <binayakpurohit007@gmail.com>
2023-06-23 17:01:43 +0530

    some new files added

---
+++ Core Java/src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        // we can write comments using double forward slash // or /* for multiline comments */
+
+        String name = "Binayak Purohit"; // Here name is the variable name
+        System.out.println(name);
+
+        long mobile = 7788881150L; // Here mobile is the variable name which stores a mobile number
+        System.out.println(mobile);
+    }
+}
